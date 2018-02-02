4549e960b73851bf1c6ab6fce041873509b962d2
Hebert Romero <hjr@iqthink.com>
2018-02-01 21:08:21 -0500

    ajustes  de btn pause play animals app

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,10 +11,52 @@ import { Animal } from '../../models/animal.interface';
 export class HomePage {
 
   public animales: Animal[] = [];
+  public audio = new Audio();
+  public audioTiempo : any;
 
   constructor(public navCtrl: NavController) {
         this.animales = ANIMALES.slice(0);
 
   }
+
+
+  reproducir( animal:Animal){
+
+          this.pausarAudio( animal );
+
+          if(animal.reproduciendo ){
+             animal.reproduciendo = false;
+             return;
+          }
+
+          let audio = new Audio();
+          this.audio.src = animal.audio;
+
+          this.audio.load();
+          this.audio.play();
+
+          animal.reproduciendo = true;
+
+
+          setTimeout( () =>  animal.reproduciendo = false, animal.duracion * 1000 );
+
+
+  }
+
+  private pausarAudio( animalSel: Animal ){
+
+          clearTimeout( this.audioTiempo );
+
+          this.audio.pause();
+          this.audio.currentTime = 0;
+
+          for( let animal of  this.animales){
+               if(animal.nombre != animalSel.nombre ){
+                    animal.reproduciendo = false;
+               }
+          }
+
+  }
+
 
 }
